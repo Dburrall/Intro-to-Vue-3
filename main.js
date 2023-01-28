fd4efbbb9b1e13d53cd2359d666e223183ab1d71
Dburrall <danburrall@gmail.com>
2023-01-28 15:45:31 -0500

    added description object for product

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data: function(){
         return {
-            product: 'Socks'
+            product: 'Socks',
+            description: 'Some of the best socks you will ever touch'
         }
     }
 })
